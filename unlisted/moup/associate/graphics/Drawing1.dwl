f394c046d795d5320b5e54ce6458ea32989ff1e5
texanmason <texanmason@gmail.com>
2020-02-27 10:59:14 -0600

    remove unlisted

--- unlisted/moup/associate/graphics/Drawing1.dwl
+++
@@ -1,3 +0,0 @@
-gjagush
-HZI3174 
-Tuesday, January 7, 2020  12:00:27 PM
--- unlisted/python/commaCode.py
+++
@@ -1,7 +0,0 @@
-spam = ['apples', 'bananas', 'tofu', 'cats']
-
-for i in range(0,len(spam)):
-	if i < len(spam)-1:
-		print(spam[i], end=", ")
-	else:
-		print("and " + spam[i])
